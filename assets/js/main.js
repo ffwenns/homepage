0390ca396771b777912be3d79157faef846e8091
manu <man.koell@gmail.com>
2025-08-21 22:42:09 +0200

    animate counters

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,22 @@
 import { FsLightbox } from "fslightbox";
+import { CountUp } from "countup.js";
 
 console.log("Created with Hugo and Tailwind CSS");
+
+document.addEventListener("DOMContentLoaded", () => {
+  const counters = document.querySelectorAll(".counter");
+
+  counters.forEach((counter, index) => {
+    const targetCount = parseInt(counter.textContent);
+    const countUp = new CountUp(counter, targetCount, {
+      startVal: 0,
+      duration: 10,
+      separator: ".",
+      enableScrollSpy: true,
+      scrollSpyOnce: true,
+      scrollSpyDelay: 100,
+    });
+
+    countUp.start();
+  });
+});
