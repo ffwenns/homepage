00506928130e0fed8d2792702c283fbeb3f2521a
manu <man.koell@gmail.com>
2025-08-22 01:28:02 +0200

    add swiper slider to home page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,33 @@
 import { FsLightbox } from "fslightbox";
 import { CountUp } from "countup.js";
+import Swiper from "swiper";
+import { Mousewheel, Navigation } from "swiper/modules";
 
 console.log("Created with Hugo and Tailwind CSS");
 
 document.addEventListener("DOMContentLoaded", () => {
+  const swiper = new Swiper(".swiper", {
+    modules: [Mousewheel, Navigation],
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    enabled: false,
+    spaceBetween: 16,
+    mousewheel: true,
+    grabCursor: true,
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        enabled: true,
+      },
+      1280: {
+        slidesPerView: 3,
+        enabled: true,
+      },
+    },
+  });
+
   const counters = document.querySelectorAll(".counter");
 
   counters.forEach((counter, index) => {
