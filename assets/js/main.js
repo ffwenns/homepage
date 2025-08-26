61f7e4e4f34939aaf82b003f1b491a118e3f5e0d
manu <man.koell@gmail.com>
2025-08-26 04:54:55 +0200

    remove swiper slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,9 @@
 import { FsLightbox } from "fslightbox";
 import { CountUp } from "countup.js";
-import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
 
 console.log("Created with Hugo and Tailwind CSS");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const swiper = new Swiper(".swiper", {
-    modules: [Navigation],
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    enabled: false,
-    spaceBetween: 16,
-    grabCursor: true,
-    breakpoints: {
-      640: {
-        slidesPerView: 2,
-        enabled: true,
-      },
-      1280: {
-        slidesPerView: 3,
-        enabled: true,
-      },
-    },
-  });
-
   const counters = document.querySelectorAll(".counter");
 
   counters.forEach((counter, index) => {
